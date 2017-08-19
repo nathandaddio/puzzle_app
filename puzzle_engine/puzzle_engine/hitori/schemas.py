aded545845eecb275e792c083b9b3f4b07e05e4c
Nathan D'Addio <nathan.daddio@biarri.com>
2017-08-19 19:03:15 +1000

    Schemas and validation

---
+++ puzzle_engine/puzzle_engine/hitori/schemas.py
@@ -0,0 +1,48 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+    post_load,
+    validates_schema,
+    ValidationError
+)
+
+from puzzle_engine.hitori.models import (
+    Board,
+    Cell
+)
+
+
+nonnegative = validate.Range(min=0)
+
+
+class CellSchema(Schema):
+    row_number = fields.Int(required=True, validate=nonnegative)
+    column_number = fields.Int(required=True, validate=nonnegative)
+    value = fields.Int(required=True)
+
+    @post_load
+    def load_cell(self, data):
+        return Cell(**data)
+
+
+class BoardSchema(Schema):
+    number_of_rows = fields.Int(required=True, validate=nonnegative)
+    number_of_columns = fields.Int(required=True, validate=nonnegative)
+    cells = fields.Nested(CellSchema, many=True, required=True)
+
+    @post_load
+    def load_board(self, data):
+        return Board(**data)
+
+    @validates_schema(skip_on_field_errors=True)
+    def check_cells_are_within_bounds(self, data):
+        for cell in data['cells']:
+            within_bounds = (
+                cell.row_number < data['number_of_rows'] and
+                cell.column_number < data['number_of_columns']
+            )
+            if not within_bounds:
+                raise ValidationError(
+                    "{} is not within the bounds of the board of ({},{})".format(
+                        cell, data['number_of_rows'], data['number_of_columns']))
---
+++ puzzle_engine/puzzle_engine/tests/hitori/test_schemas.py
@@ -0,0 +1,208 @@
+import pytest
+import mock
+
+
+from marshmallow import ValidationError
+
+
+from puzzle_engine.hitori.schemas import (
+    CellSchema,
+    BoardSchema
+)
+
+
+class TestCellSchema:
+    @pytest.fixture
+    def data(self):
+        return {
+            'row_number': 1,
+            'column_number': 2,
+            'value': 5
+        }
+
+    @pytest.fixture
+    def patched_cell(self):
+        patcher = mock.patch('puzzle_engine.hitori.schemas.Cell')
+        yield patcher.start()
+        patcher.stop()
+
+    def test_cell_schema_loads(self, data, patched_cell):
+        loaded_data = CellSchema(strict=True).load(data).data
+
+        assert loaded_data is patched_cell.return_value
+        patched_cell.assert_called_once_with(**data)
+
+    bad_data = [
+        {
+            'row_number': 3,
+            'column_number': -1,
+            'value': 5
+        },
+        {
+            'row_number': -3,
+            'column_number': 5,
+            'value': 5
+        },
+        {
+            'row_number': -3,
+            'column_number': -5,
+            'value': 2
+        }
+    ]
+
+    @pytest.mark.parametrize('data', bad_data)
+    def test_cell_schema_validates(self, data):
+        with pytest.raises(ValidationError):
+            CellSchema(strict=True).load(data)
+
+
+class TestBoardSchema:
+    @pytest.fixture
+    def data(self):
+        return {
+            'number_of_rows': 5,
+            'number_of_columns': 5,
+            'cells': [
+                {
+                    'row_number': 1,
+                    'column_number': 2,
+                    'value': 3
+                }
+            ]
+        }
+
+    @pytest.fixture
+    def patched_board(self):
+        patcher = mock.patch('puzzle_engine.hitori.schemas.Board')
+        yield patcher.start()
+        patcher.stop()
+
+    def test_board_schema_loads(self, data, patched_board):
+        loaded_data = BoardSchema(strict=True).load(data).data
+
+        assert patched_board.return_value is loaded_data
+
+        call = patched_board.call_args[1]
+        assert call['number_of_rows'] == data['number_of_rows']
+        assert call['number_of_columns'] == data['number_of_columns']
+
+        assert call['cells']
+        assert len(call['cells']) == 1
+
+        cell = call['cells'][0]
+
+        assert cell.row_number == 1
+        assert cell.column_number == 2
+        assert cell.value == 3
+
+    bad_data = [
+        {
+            'number_of_rows': -5,
+            'number_of_columns': 5,
+            'cells': [
+                {
+                    'row_number': 1,
+                    'column_number': 2,
+                    'value': 3
+                }
+            ]
+        },
+        {
+            'number_of_rows': 5,
+            'number_of_columns': -5,
+            'cells': [
+                {
+                    'row_number': 1,
+                    'column_number': 2,
+                    'value': 3
+                }
+            ]
+        },
+        {
+            'number_of_rows': -5,
+            'number_of_columns': -5,
+            'cells': [
+                {
+                    'row_number': 1,
+                    'column_number': 2,
+                    'value': 3
+                }
+            ]
+        },
+        {
+            'number_of_rows': 5,
+            'number_of_columns': 5,
+            'cells': [
+                {
+                    'row_number': 10,
+                    'column_number': 2,
+                    'value': 3
+                }
+            ]
+        },
+        {
+            'number_of_rows': 5,
+            'number_of_columns': 5,
+            'cells': [
+                {
+                    'row_number': 3,
+                    'column_number': 12,
+                    'value': 3
+                }
+            ]
+        },
+        {
+            'number_of_rows': 5,
+            'number_of_columns': 5,
+            'cells': [
+                {
+                    'row_number': 10,
+                    'column_number': 12,
+                    'value': 3
+                }
+            ]
+        },
+        {
+            'number_of_rows': 5,
+            'number_of_columns': 5,
+            'cells': [
+                {
+                    'row_number': 1,
+                    'column_number': 6,
+                    'value': 3
+                },
+                {
+                    'row_number': 3,
+                    'column_number': 2,
+                    'value': 3
+                }
+            ]
+        },
+        {
+            'number_of_rows': 5,
+            'number_of_columns': 5,
+            'cells': [
+                {
+                    'row_number': 5,
+                    'column_number': 3,
+                    'value': 3
+                }
+            ]
+        },
+        {
+            'number_of_rows': 5,
+            'number_of_columns': 5,
+            'cells': [
+                {
+                    'row_number': 3,
+                    'column_number': 5,
+                    'value': 3
+                }
+            ]
+        },
+    ]
+
+    @pytest.mark.parametrize('data', bad_data)
+    def test_board_schema_validates(self, data):
+        with pytest.raises(ValidationError):
+            BoardSchema(strict=True).load(data)
